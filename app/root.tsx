e57ce32a628c28e6edf84feb32513789565efed3
Fernando <esponges@gmail.com>
2022-02-12 19:28:34 -0600

    refactor root files

--- app/root.tsx
+++ app/root.tsx
@@ -1,81 +1,8 @@
-import {
-  Links,
-  LiveReload,
-  LoaderFunction,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-} from "remix";
-import type { MetaFunction } from "remix";
-import { Link } from "react-router-dom";
-import { getUser } from "./utils/session.server";
-import { User } from "@prisma/client";
-import { StoreProvider } from "./store/context/context";
-import { db } from "./utils/db.server";
+import Component, {
+  ErrorBoundary,
+  CatchBoundary,
+  meta,
+} from '~/route-containers/layout/layout.component';
 
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
-
-type LoaderData = {
-  user: User | null;
-  products: {id: string, createdAt: string, updatedAt: string, name: string, description: string, discount: number, imageUrl: string, isAvailable: boolean, isFeatured: boolean, price: number}[];
-};
-
-// init data
-export let loader: LoaderFunction = async ({ request }) => {
-  let user = await getUser(request);
-  const products = await db.product.findMany();
-  return { user: user?.username, products };
-};
-
-function App() {
-  const data = useLoaderData<LoaderData>();
-
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <StoreProvider initData={data}>
-        <body>
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-          />
-          <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-                {data.user ? (
-                  <div className="user-info">
-                    <span>{`Hi ${data.user.username}`}</span>
-                    <form action="/logout" method="post">
-                      <button type="submit" className="button">
-                        Logout
-                      </button>
-                    </form>
-                  </div>
-                ) : (
-                  <Link to="/login">Login</Link>
-                )}
-              </li>
-            </ul>
-          </nav>
-          <Outlet />
-          <ScrollRestoration />
-          <Scripts />
-          {process.env.NODE_ENV === "development" && <LiveReload />}
-        </body>
-      </StoreProvider>
-    </html>
-  );
-}
-
-export default App;
+export default Component;
+export { ErrorBoundary, CatchBoundary, meta };
---
+++ app/route-containers/layout/layout.component.tsx
@@ -0,0 +1,93 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from "remix";
+import type { MetaFunction } from "remix";
+import { Link } from "react-router-dom";
+import { LoaderData } from "~/store/initialData.server";
+import { StoreProvider } from "~/store/context/context";
+import { GenericErrorBoundary } from "../boundaries/generic-error-boundary";
+import { GenericCatchBoundary } from "../boundaries/generic-catch-boundary";
+
+export const meta: MetaFunction = () => {
+  return { title: "New Remix App" };
+};
+// init data
+
+export function App({ children, data }: { children: React.ReactNode, data?: LoaderData }) {
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <StoreProvider initData={data}>
+        <body>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+          />
+          <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+                <Link to="/hello">Hello</Link>
+                {data?.user ? (
+                  <div className="user-info">
+                    <span>{`Hi ${data.user}`}</span>
+                    <form action="/logout" method="post">
+                      <button type="submit" className="button">
+                        Logout
+                      </button>
+                    </form>
+                  </div>
+                ) : (
+                  <Link to="/login">Login</Link>
+                )}
+              </li>
+            </ul>
+          </nav>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+          {process.env.NODE_ENV === "development" && <LiveReload />}
+        </body>
+      </StoreProvider>
+    </html>
+  );
+};
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <App>
+      <GenericErrorBoundary error={error} />
+    </App>
+  );
+};
+
+export function CatchBoundary() {
+  return (
+    <App>
+      <GenericCatchBoundary />
+    </App>
+  );
+};
+
+export default function Root() {
+  const data = useLoaderData();
+
+    return (
+        <App data={data}>
+            <Outlet />
+        </App>
+    );
+};
