86e90ef601afdb013285d6947f5652c8633dd32f
Fernando <esponges@gmail.com>
2022-02-20 18:48:00 -0600

    refactor reducers

--- app/store/reducers/products.tsx
+++ app/store/reducers/products.tsx
@@ -1,19 +1,36 @@
-import type { State } from "../context/context";
-
-type Action = {
-    type: 'SET_PRODUCTS'
-    payload: [any]
+export enum Actions {
+  SET_PRODUCTS = 'SET_PRODUCTS',
+  SET_CART = 'SET_CART',
 }
 
-export function productsReducer(state: State, action: Action) {
-    switch (action.type) {
-      case 'SET_PRODUCTS': {
-        return { ...state, products: action.payload}
-      }
-      default: {
-        return state
-      }
+type Action = {
+  type: Actions,
+  payload: any;
+};
+
+export function productsReducer(state: any, action: Action) {
+  switch (action.type) {
+    case Actions.SET_PRODUCTS: {
+      console.log('productsReducer', action.payload);
+      return { ...state, products: action.payload };
+    }
+    default: {
+      return state;
     }
   }
+}
 
-export type { Action }
+export function cartItemsReducer(state: any, action: Action) {
+  console.log(action.payload);
+  switch (action.type) {
+    case Actions.SET_CART: {
+      console.log('set cart', action.payload);
+      return { ...state, cartItems: action.payload };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export type { Action };
