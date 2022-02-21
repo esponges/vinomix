81feb14e0593b62b160679e1cc7d219bfb63cd7e
Fernando <esponges@gmail.com>
2022-02-20 18:48:35 -0600

    import component and test action fns

--- app/routes/product/$productId.tsx
+++ app/routes/product/$productId.tsx
@@ -1,8 +1,9 @@
-import { LoaderFunction, redirect } from "remix/server";
+import { ActionFunction, LoaderFunction, redirect } from "remix/server";
 import { db } from "~/utils/db.server";
 import { useLoaderData } from "remix";
 import { Product } from "@prisma/client";
-import { Button, Grid, Header, Message } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
+import { ProductCard } from "~/components/UI/organisms/product-card";
 
 export const loader: LoaderFunction = async ({ params }) => {
   const product = await db.product.findFirst({
@@ -11,28 +12,30 @@ export const loader: LoaderFunction = async ({ params }) => {
   if (!product) {
     return redirect("/");
   }
-  console.log(product);
   return product;
 };
 
+export const action: ActionFunction = async ({ request }) => { 
+    // could have used params to fetch the product id,
+    // but we can also use the request to get the product id
+    // by using the form value from _action
+    const formData = await request.formData();
+    const { _action } = Object.fromEntries(formData);
+
+    const product = await db.product.findFirst({
+        where: { id: _action.toString() },
+    });
+    console.log('action', Object.fromEntries(formData), product)
+    
+    return true;
+};       
+
 export default function ProductDetails() {
   const product = useLoaderData<Product>();
-  console.log(product.name, product.description, product);
 
   return (
-    <div>
-      <Grid container>
-        <Grid.Row>
-          <Grid.Column>
-            <Message>
-              <Header as="h1">{product.name}</Header>
-              <p>{product.description}</p>
-              <Button color="blue">Learn more &raquo;</Button>
-              <h1></h1>
-            </Message>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </div>
+    <Container>
+      <ProductCard product={product} />
+    </Container>
   );
 }
